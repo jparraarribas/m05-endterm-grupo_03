dd81c9c97cc61092ef392209334bf305fbc39dd0
SilviaBlanco <sblanco@fp.lasalle.es>
2026-04-12 11:36:45 +0200

    Se genera y se mueve de sitio TestVehiculo.java para que sea reconocido como proyecto Maven. Todavía no funciona porque no he creado la clase Vehiculo

---
+++ vehiculo/src/main/resources/archetype-resources/src/test/java/TestVehiculo.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehiculoTest {
+
+    @Test
+    void acelerar_deberiaAumentarVelocidad() {
+        Vehiculo v = new Vehiculo();
+        v.acelerar(10);
+        assertEquals(10, v.getVelocidad());
+    }
+}
