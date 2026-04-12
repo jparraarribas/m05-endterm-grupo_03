91ad071760a6d2caf337a780d7af7b289bbaf73d
SilviaBlanco <sblanco@fp.lasalle.es>
2026-04-12 16:57:16 +0200

    El test VehiculoTest acelerar_deberiaAumentarVelocidad() ya pasa sin fallos

--- vehiculo/src/test/java/org/example/Vehiculo.java
+++ vehiculo/src/test/java/org/example/Vehiculo.java
@@ -1,5 +1,14 @@
 package org.example;
 
 public class Vehiculo {
+    private int velocidad = 0;
 
+    public void acelerar(int cantidad) {
+        velocidad += cantidad;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+
+    }
 }
